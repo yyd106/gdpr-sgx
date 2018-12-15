8a3320e5baa2e2cade0a545b08f6bbfc6b83f2ce
TonyCode2012 <TonyCode2012@126.com>
2018-12-16 01:01:44 +0800

    add java project EnclaveBridge

---
+++ tools/EnclaveBridge/src/main/java/TestEnclaveBridge.java
@@ -0,0 +1,17 @@
+import Messages.Messages;
+import com.google.protobuf.InvalidProtocolBufferException;
+
+public class TestEnclaveBridge
+{
+    public static void main(String[] args) {
+        EnclaveBridge eb = new EnclaveBridge();
+        String res = eb.handleVerification(eb.messageHandlerOBJ);
+        try {
+            byte[] data = res.getBytes();
+            Messages.MessageMsg0 result = Messages.MessageMsg0.parseFrom(data);
+            System.out.println(result.getEpid());
+        } catch (InvalidProtocolBufferException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
