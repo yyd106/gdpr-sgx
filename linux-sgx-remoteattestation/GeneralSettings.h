47dc6ae10a0cf0326cb5aadba14f3239e1fda6c2
TonyCode2012 <TonyCode2012@126.com>
2018-12-16 00:59:57 +0800

    refactor framework and complete protobuf serialization and deserialization

--- linux-sgx-remoteattestation/GeneralSettings.h
+++ linux-sgx-remoteattestation/GeneralSettings.h
@@ -9,13 +9,13 @@ namespace Settings {
 	static int rh_port = 22222;
 	static string rh_host = "localhost";
 	
-	static string server_crt = "../cert/real/client.crt"; //certificate for the HTTPS connection between the SP and the App
-	static string server_key = "../cert/real/client.key"; //private key for the HTTPS connection
-	//static string server_crt = "../cert/tmp/server.crt"; //certificate for the HTTPS connection between the SP and the App
-	//static string server_key = "../cert/tmp/server.key"; //private key for the HTTPS connection
+	//static string server_crt = "../cert/real/client.crt"; //certificate for the HTTPS connection between the SP and the App
+	//static string server_key = "../cert/real/client.key"; //private key for the HTTPS connection
+	static string server_crt = "../cert/tmp/server.crt"; //certificate for the HTTPS connection between the SP and the App
+	static string server_key = "../cert/tmp/server.key"; //private key for the HTTPS connection
 
 	static string spid = "4F2229E41A0ECA12BAE6EDB36E87EAFE"; //SPID provided by Intel after registration for the IAS service
-	static const char *ias_crt = "../cert/real/client.crt"; //location of the certificate send to Intel when registring for the IAS
+	static const char *ias_crt = "../cert/real/client.pem"; //location of the certificate send to Intel when registring for the IAS
 	//static string ias_url = "https://test-as.sgx.trustedservices.intel.com:443/attestation/sgx/v1/";
 	static string ias_url = "https://test-as.sgx.trustedservices.intel.com:443/attestation/sgx/v3/";
     static const char *ec_pri_key_path = "./ec256_private_key";
