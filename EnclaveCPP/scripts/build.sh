6eebdc05c031a8c670fdab36931819c76f888ba4
TonyCode2012 <TonyCode2012@126.com>
2018-12-20 21:46:15 +0800

    fix EncalveCPP build script

--- EnclaveCPP/scripts/build.sh
+++ EnclaveCPP/scripts/build.sh
@@ -2,6 +2,11 @@
 usage()
 {
 cat << EOF
+    build.sh [option] -l <jnilib_des_path> -j <javafile_des_path>
+        options:
+                -r: indicate rebuilding java file to create new c header file
+                -l: the destination path of the new created jni library
+                -j: the destination path of the new java file
 EOF
 }
 
@@ -10,7 +15,7 @@ verbose()
     local type=$1
     local info=$2
     if [ x"$type" = x"ERROR" ]; then
-        echo -e "${RED}`date "+%s"` $type $info${NC}" >&2
+        echo -e "${RED}`date "+%s"` $type $info${NC}" >&2 
         return
     fi
     echo -e "${GREEN}`date "+%s"` $type $info${NC}"
@@ -23,8 +28,8 @@ basedir=`cd $basedir; pwd`
 target="libEnclaveBridge.so"
 javafile="EnclaveBridge.java"
 headerfile="EnclaveBridge.h"
-jnidir=$basedir/../../tools/EnclaveBridge/src/main/jnilib
-javadir=$basedir/../../tools/EnclaveBridge/src/main/java
+jnidir=`cd $basedir/../../tools/EnclaveBridge/src/main/jnilib; pwd`
+javadir=`cd $basedir/../../tools/EnclaveBridge/src/main/java; pwd`
 homedir=$basedir/..
 
 RED='\033[0;31m'
@@ -33,19 +38,20 @@ NC='\033[0m'
 
 rebuild="no"
 
-CMD=`getopt -o rl:j: -a -l rebuild,jnilib:,javafile: -n "usage" -- "$@"`
+CMD=`getopt -o "rl:j:" -a -l "rebuild,jnilib:,javafile:" -n "usage" -- "$@"`
 if [ $? != 0 ]; then
     verbose ERROR "Parameter error, terminate" >&2
     exit 1
 fi
+
 
 eval set -- "$CMD"
 
 while true; do
     case "$1" in
         -r|--rebuild|-rebuild)
-            rebuild=$2
-            shift 2
+            rebuild="yes"
+            shift
             ;;
         -l|--jnilib|-jnilib)
             jnidir=$2
@@ -70,12 +76,12 @@ done
 cd $homedir
 
 if [ x"$rebuild" = x"yes" ]; then
-    if [ -d "$jnidir" ]; then
-        verbose ERROR "JNI directory doesn't exist"
+    if [ ! -d "$jnidir" ]; then
+        verbose ERROR "JNI directory($jnidir) doesn't exist"
         exit 1
     fi
-    if [ -d "$javadir" ]; then
-        verbose ERROR "JAVA directory doesn't exist"
+    if [ ! -d "$javadir" ]; then
+        verbose ERROR "JAVA directory($javadir) doesn't exist"
         exit 1
     fi
     javac $javafile
