398b2b5d284a817555b0bb7af05471247578d076
TonyCode2012 <TonyCode2012@126.com>
2019-01-09 23:29:50 +0800

    fix build_targets script

--- scripts/build_targets.sh
+++ scripts/build_targets.sh
@@ -57,7 +57,7 @@ function buildEnclaveJNI()
 {
     cd $jnidir
     verbose INFO "Building jni library..."
-    ./scripts/build.sh -d "$targetdir"
+    ./scripts/build.sh -d "$targetdir" -m SIM
     cd -
 }
 
